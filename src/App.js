feb7886e82eff65a7bb5aa906ef629e80450cfc8
vlasnikita <vlasenkonikita@gmail.com>
2017-09-23 01:34:43 +0300

    Unidirectional data flow, filterable input search added

--- src/App.js
+++ src/App.js
@@ -20,11 +20,45 @@ const list = [
   },
 ];
 
+const isSearched = (searchTerm) => (item) => 
+    !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    
+    this.state = {
+      list: list,
+      searchTerm: '',
+    };
+
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onSearchChange(event){
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  onDismiss(id){
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
+  }
+
+
   render() {
+    const { searchTerm, list } = this.state;
     return (
-      <div className="App">       
-          {list.map(item => 
+      <div className="App">     
+        <form action="">
+            <input 
+            type="text"
+            onChange={this.onSearchChange}
+            />
+          </form>  
+          {list.filter(isSearched(searchTerm)).map(item => 
               <div key={item.objectID}>
                 <span>
                   <a href={item.url}>{item.title}</a>
@@ -32,6 +66,14 @@ class App extends Component {
                 <span>{item.author}</span>
                 <span>{item.num_comments}</span>
                 <span>{item.points}</span>
+                <span>
+                  <button
+                  onClick={() => this.onDismiss(item.objectID)}
+                  type="button"
+                  >
+                  Dismiss
+                  </button>
+                </span>
               </div>
           )}
       </div>
